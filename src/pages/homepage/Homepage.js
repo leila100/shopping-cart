e2c84293518fd90be19da72d6ae8e649f36d8d17
Leila <nb.leila10@gmail.com>
2020-03-20 13:04:22 -0500

    Resctructure homepage into a directory component

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,18 +1,12 @@
 import React from "react";
 
 import "./homepage.styles.scss";
-import MenuItem from "../../components/menu-item/menuItem";
+import Directory from "../../components/directory/Directory";
 
 const Homepage = () => {
   return (
     <div className='homepage'>
-      <div className='directory-menu'>
-        <MenuItem item='HATS' />
-        <MenuItem item='JACKETS' />
-        <MenuItem item='SNEAKERS' />
-        <MenuItem item='MEN' />
-        <MenuItem item='WOMEN' />
-      </div>
+      <Directory />
     </div>
   );
 };
