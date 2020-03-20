2aa3485d5c0df5263748d4a8479381c19a6d586e
Leila <nb.leila10@gmail.com>
2020-03-20 13:18:23 -0500

    Add images to the directory masnu items

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -47,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem title={title} imageUrl={imageUrl} id={id} />
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem title={title} imageUrl={imageUrl} id={id} size={size} />
         ))}
       </div>
     );
--- src/components/menu-item/menuItem.js
+++ src/components/menu-item/menuItem.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import "./menuItem.styles.scss";
 
-const MenuItem = ({ item }) => {
+const MenuItem = ({ item, imageUrl, size }) => {
   return (
-    <div className='menu-item'>
+    <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imageUrl})` }}>
       <div className='content'>
         <h1 className='title'>{item}</h1>
         <span className='subtitle'>SHOP NOW</span>
