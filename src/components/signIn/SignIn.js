2f3a83901c2c4b630c49fa3a21d1ac30ad225a2d
Leila <nb.leila10@gmail.com>
2020-03-21 20:20:31 -0500

    Add signUp page and save new user to the database

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.state({ email: "", password: "" });
+    this.setState({ email: "", password: "" });
   };
 
   handleChange = event => {
--- src/pages/signIn-signUp/SignIn_SignUp.js
+++ src/pages/signIn-signUp/SignIn_SignUp.js
@@ -3,11 +3,13 @@ import React from "react";
 import "./signIn-signUp.styles.scss";
 
 import SignIn from "../../components/signIn/SignIn";
+import SignUp from "../../components/signUp/SignUp";
 
 const SignIn_SignUp = () => {
   return (
     <div className='sign-in-and-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
