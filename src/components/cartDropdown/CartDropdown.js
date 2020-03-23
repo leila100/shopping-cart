e57af363769bd1b96689f2fd2ddb0609bf2c26e9
Leila <nb.leila10@gmail.com>
2020-03-23 18:52:42 -0500

    Create cartItem and display it in dropdown when adding to cart

--- src/components/cartDropdown/CartDropdown.js
+++ src/components/cartDropdown/CartDropdown.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CustomButton from "../customButton/CustomButton";
+import CartItem from "../cartItem/CartItem";
 
 import "./cartDropdown.styles.scss";
 
-const CartDropdowm = () => {
+const CartDropdowm = ({ cartItems }) => {
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items'></div>
+      <div className='cart-items'>
+        {cartItems.map(item => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default CartDropdowm;
+const mapStateToProps = state => ({
+  cartItems: state.cart.cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdowm);
