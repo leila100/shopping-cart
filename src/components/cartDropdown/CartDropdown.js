c18784ad9f15ecd8b61758a3fd4fa252f5a07aea
Leila <nb.leila10@gmail.com>
2020-03-23 19:49:58 -0500

    Add createStructuredSelector in cartIcon and cartDropdown

--- src/components/cartDropdown/CartDropdown.js
+++ src/components/cartDropdown/CartDropdown.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import CustomButton from "../customButton/CustomButton";
 import CartItem from "../cartItem/CartItem";
@@ -20,8 +21,8 @@ const CartDropdowm = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = state => ({
-  cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
 });
 
 export default connect(mapStateToProps)(CartDropdowm);
