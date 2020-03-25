47d5868d8872123ae31adf075d482842036eafa5
Leila <nb.leila10@gmail.com>
2020-03-24 20:24:34 -0500

    Add checkout header

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -1,9 +1,29 @@
 import React from "react";
 
-// import "checkout.styles.scss";
+import "./checkout.styles.scss";
 
 const Checkout = () => {
-  return <div>Checkout page</div>;
+  return (
+    <div className='checkout-page'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
