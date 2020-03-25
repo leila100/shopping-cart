1e94d6458e7ab804670e3befaae25301073d958a
Leila <nb.leila10@gmail.com>
2020-03-25 18:23:27 -0500

    Display collection items in corresponding collection page

--- src/pages/collection/Collection.js
+++ src/pages/collection/Collection.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
-// import { createStructuredSelector } from "reselect";
 
-// import {} from "../../redux/shop/shopSelectors"
-// import CollectionItem from "../../components/collectionItem/CollectionItem";
+import { selectCollection } from "../../redux/shop/shopSelectors";
+import CollectionItem from "../../components/collectionItem/CollectionItem";
 
 import "./collection.styles.scss";
 
-const Collection = ({ match }) => {
+const Collection = ({ match, collection }) => {
   return (
-    <div className='category'>
-      <h2>Collection {match.params.collectionId} page</h2>
+    <div className='collection-page'>
+      {collection.items.map(item => (
+        <CollectionItem key={item.id} item={item} />
+      ))}
     </div>
   );
 };
 
-// const mapStateToProps = createStructuredSelector({});
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
 
-export default connect()(Collection);
+export default connect(mapStateToProps)(Collection);
