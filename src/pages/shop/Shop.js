f1757933a9104cc61f0583e105c1ff0b3ea48da5
Leila <nb.leila10@gmail.com>
2020-03-25 17:39:13 -0500

    Put the shop page in its own component: CollectionOverview

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,23 +1,13 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import { selectCollections } from "../../redux/shop/shopSelectors";
-
-import CollectionPreview from "../../components/collectionPreview/CollectionPreview";
+import CollectionsOverview from "../../components/collectionsOverview/CollectionsOverview";
 
 const Shop = ({ collections }) => {
   return (
     <div>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(Shop);
+export default Shop;
