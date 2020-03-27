02eed0165651b3e4556eebefd662d9a774e14e4e
Leila <nb.leila10@gmail.com>
2020-03-26 21:29:12 -0500

    Convert Custom button component styles to styled-components

--- src/components/customButton/CustomButton.js
+++ src/components/customButton/CustomButton.js
@@ -1,16 +1,9 @@
 import React from "react";
 
-import "./customButton.styles.scss";
+import { CustomButtonContainer } from "./customButton.styles";
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
-  return (
-    <button
-      className={`${inverted ? "inverted" : ""} ${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+const CustomButton = props => {
+  return <CustomButtonContainer {...props}>{props.children}</CustomButtonContainer>;
 };
 
 export default CustomButton;
