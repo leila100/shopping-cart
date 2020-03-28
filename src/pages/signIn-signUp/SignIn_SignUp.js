eef8d58b5c8e94be5369fc08a1e5d3998479a667
Leila <nb.leila10@gmail.com>
2020-03-27 20:52:05 -0500

    convert styles for the Sign-in Sign-up page to styled-components

--- src/pages/signIn-signUp/SignIn_SignUp.js
+++ src/pages/signIn-signUp/SignIn_SignUp.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import "./signIn-signUp.styles.scss";
-
 import SignIn from "../../components/signIn/SignIn";
 import SignUp from "../../components/signUp/SignUp";
 
+import { SignInSignUpContainer } from "./signInSignUp.styles";
+
 const SignIn_SignUp = () => {
   return (
-    <div className='sign-in-and-sign-up'>
+    <SignInSignUpContainer>
       <SignIn />
       <SignUp />
-    </div>
+    </SignInSignUpContainer>
   );
 };
 
