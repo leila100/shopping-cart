419647966baaabaea13d92971888109717e517de
Leila <nb.leila10@gmail.com>
2020-04-13 17:52:09 -0500

    Resctructure the shop sagas using the yield all

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,9 +1,9 @@
 import { all, call } from "redux-saga/effects";
 
-import { fetchCollectionsStart } from "./shop/shopSagas";
 import { userSagas } from "./user/userSagas";
 import { cartSagas } from "./cart/cartSagas";
+import { shopSagas } from "./shop/shopSagas";
 
 export default function* rootSaga() {
-  yield all([call(fetchCollectionsStart), call(userSagas), call(cartSagas)]);
+  yield all([call(userSagas), call(cartSagas), call(shopSagas)]);
 }
