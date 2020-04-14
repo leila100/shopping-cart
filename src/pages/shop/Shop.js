750ee43a9028cc5cd57cfaec9dc51d742c68f2ee
Leila <nb.leila10@gmail.com>
2020-04-14 18:35:15 -0500

    Update the Shop component to use useEffect hook

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -7,21 +7,18 @@ import { fetchCollectionsStart } from "../../redux/shop/shopActions";
 import CollectionOverviewContainer from "../../components/collectionsOverview/CollectionsOverviewContainer";
 import CollectionContainer from "../collection/CollectionContainer";
 
-class Shop extends React.Component {
-  componentDidMount() {
-    this.props.fetchCollectionsStart();
-  }
+const Shop = ({ match, fetchCollectionsStart }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-    return (
-      <div>
-        <Route exact path={match.path} component={CollectionOverviewContainer} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionContainer} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Route exact path={match.path} component={CollectionOverviewContainer} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionContainer} />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
