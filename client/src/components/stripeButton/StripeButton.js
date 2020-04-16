87d1872240bd9b1174f8ad299d698124841d3541
Leila <nb.leila10@gmail.com>
2020-04-15 22:21:45 -0500

    Connect the stripe button to the server to make payments

--- client/src/components/stripeButton/StripeButton.js
+++ client/src/components/stripeButton/StripeButton.js
@@ -1,13 +1,25 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_lTb9H3cC6IOEaOWOPPXgc2Bp00gR4r5Jhg";
 
-  const onToken = token => {
-    console.log(token);
-    alert("Payment successful");
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => alert("Payment Successful!"))
+      .catch((error) => {
+        console.log("Payment Error: ", JSON.parse(error));
+        alert("There was an issue processing your payment. please be sure you use the provided credit card.");
+      });
   };
 
   return (
